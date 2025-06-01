695595792f55f897b83837aeeb99836bb0a13671
butcho <skeletorkoltmm@gmail.com>
2025-06-02 01:29:48 +0300

    path validator fix

--- src/path_explorator/core/validator.py
+++ src/path_explorator/core/validator.py
@@ -4,21 +4,20 @@ from pathlib import Path
 
 
 class PathValidator:
+
+    def __init__(self, root_dir):
+        self.root_dir = Path(root_dir)
+
     def clear_path(self, path: str):
         if not isinstance(path, str):
             raise TypeError(f'path arg must be str, not {type(path)}')
         return Path(path).resolve().__str__()
 
-    def is_goes_beyond_limits(self, limit_path: str, requesting_path: str):
-        if not isinstance(limit_path, str):
-            raise TypeError(f'limit path arg must be str, not {type(limit_path)}')
+    def is_goes_beyond_limits(self, requesting_path: str):
         if not isinstance(requesting_path, str):
             raise TypeError(f'requesting path arg must be str, not {type(requesting_path)}')
-        limit_path = Path(self.clear_path(limit_path))
         requesting_path = Path(self.clear_path(requesting_path))
-        if not limit_path.exists():
-            raise EntityDoesNotExists(limit_path)
-        if limit_path in requesting_path.parents or limit_path == requesting_path:
+        if self.root_dir in requesting_path.parents or self.root_dir == requesting_path:
             return False
         return True
 
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -10,8 +10,11 @@ class TestValidator(unittest.TestCase):
 
     def test_is_goes_beyond_limits(self):
         limit_path: Path = self.test_folder
+        limit_path = Path('/home/butcho/пайтон проекты/media_storage/storage')
         safe_path = Path(limit_path / Path('movies/brat.movie'))
         unsafe_path = Path(Path.cwd() / Path('__init__.py'))
+        safe_path = Path('/home/butcho/пайтон проекты/media_storage/storage')
+        unsafe_path = Path('/home/butcho/c_scratches/main.c')
         is_safe = not self.validator.is_goes_beyond_limits(limit_path.__str__(), safe_path.__str__())
         is_unsafe = self.validator.is_goes_beyond_limits(limit_path.__str__(), unsafe_path.__str__())
         self.assertTrue(is_safe)
