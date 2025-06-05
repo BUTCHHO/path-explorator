2206244be04831593a8aa9ba106f0d896f31f81a
butcho <skeletorkoltmm@gmail.com>
2025-06-05 12:21:03 +0300

    path_creator updated

--- src/path_explorator/core/explorer/path/path_creator.py
+++ src/path_explorator/core/explorer/path/path_creator.py
@@ -4,20 +4,8 @@ from pathlib import Path
 class PathCreator:
     def __init__(self):
         pass
-    
-    def smart_path(self, *paths):
-        if all(isinstance(path, str) for path in paths):
-            pass
 
-    def join_strlike_path(self, path1, path2):
-        new_path = path1 / path2
-        return new_path
-
-    def join_strlike_path_to_pathliblike(self, path1, path2):
+    def join_paths(self, path1, path2):
         new_path = Path(path1 / path2)
-        return new_path
-
-    def join_path(self, path1, path2):
-        new_path = Path(path1 / path2)
-        return new_path
+        return new_path.__str__()
 
