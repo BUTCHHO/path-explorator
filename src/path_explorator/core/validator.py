91a6af03c4f5a7390e6660d12137cead176272c0
butcho <butcho@debian-pc.net>
2025-06-25 14:52:33 +0300

    validator is_path_rel_to_another_path method added

---
+++ src/path_explorator/core/validator.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+
+
+class PathValidator:
+
+    def __init__(self, root_dir):
+        self.root_dir = Path(root_dir)
+
+    def does_contain_uplinks(self, path:str | Path):
+        path = Path(path)
+        if ('..', '.', './') in path.parts:
+            return True
+        return False
+
+    def does_contain_symlinks_and_uplinks(self, path:str):
+        path = Path(path)
+        if path.is_symlink() and self.does_contain_uplinks(path):
+            return True
+        return False
+
+
+    def is_goes_beyond_limits(self, requesting_path: str):
+        if not isinstance(requesting_path, str):
+            raise TypeError(f'requesting path arg must be str, not {type(requesting_path)}')
+        return not self.is_path_rel_to_another_path(requesting_path, self.root_dir)
+
+    def is_path_rel_to_another_path(self, relating_path: str | Path, relative_to_path: str | Path):
+        relating = Path(relating_path).resolve()
+        relative_to = Path(relative_to_path)
+        if not relating.is_relative_to(relative_to):
+            return False
+        return True
+
+    @property
+    def root(self):
+        return self.root_dir.__str__()
+
+    def is_exists(self, path: str) -> bool:
+        """
+        Checks if specified path exists
+        :param path: path to the entity checking
+        :return: exists or not True or False
+        """
+        if not isinstance(path, str):
+            raise TypeError(f'path arg must be str, not {type(path)}')
+        entity = Path(self.root_dir, path)
+        return entity.exists()
+
+
+    def is_file(self, path: str) -> bool:
+        """
+        Checks if specified entity is FILE
+        :param path: path to entity
+        :return: file or not
+        """
+        if not isinstance(path, str):
+            raise TypeError(f'path arg must be str, not {type(path)}')
+        entity = Path(self.root_dir, path)
+        return entity.is_file()
+
+
+    def is_dir(self, path: str) -> bool:
+        """
+        Checks if specified entity is DIRECTORY/FOLDER
+        :param path: path to entity
+        :return: dir or not
+        """
+        if not isinstance(path, str):
+            raise TypeError(f'path arg must be str, not {type(path)}')
+        entity = Path(self.root_dir, path)
+        return entity.is_dir()
